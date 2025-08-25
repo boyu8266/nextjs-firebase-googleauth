2a26255534a9740872caa3f5aef4bc4bfb50167e
boyu8266 <boryuh8266@gmail.com>
2025-08-25 10:46:43 +0800

    feat(ui): remove authentication check from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import { auth } from "@/app/auth";
 import { LoginHistoryHomeView } from "@/app/components/LoginHistoryHomeView";
 import TopToolbar from "@/app/components/top-toolbar";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth();
-  if (!session) {
-    redirect("/login");
-  }
-
   return (
     <main>
       <TopToolbar />
